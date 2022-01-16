b4d35b19ff76cda27a07d770db368ce55cf61226
Johannes Aberidis <jo@e175.net>
2022-01-16 14:28:31 +0100

    add test

---
+++ tests/TestCase/Decoder/DeoceRunTest.php
@@ -0,0 +1,33 @@
+<?php
+declare (strict_types = 1);
+
+/**
+ * Sportlog (https://sportlog.at)
+ *
+ * @license MIT License
+ */
+
+namespace Sportlog\FIT\Test\TestCase\Decoder;
+
+use Sportlog\FIT\Decoder;
+use Sportlog\FIT\Test\TestCase\FilePath;
+use PHPUnit\Framework\TestCase;
+use Sportlog\FIT\Profile\Messages\RecordMessage;
+use Sportlog\FIT\Profile\Types\MesgNum;
+
+/**
+ * Decodes the file 'Settings.fit'
+ * from the FIT SDK (FitSDKRelease_21.40.00)
+ */
+final class DecodeRunTest extends TestCase {
+    public function testDecodeFile(): void {
+        $decoder = new Decoder();
+        $messages = $decoder->read(FilePath::getPathTo('Run.fit'));
+
+        /** @var RecordMessage[] $records */
+        $records = $messages->getMessages(MesgNum::RECORD);
+        $this->assertCount(3155, $records);
+        $this->assertEquals('2020-12-20T10:19:53+00:00', $records[0]->getTimestamp()->format('c'));
+        $this->assertEquals('2020-12-20T11:12:27+00:00', $records[count($records)-1]->getTimestamp()->format('c'));
+    }
+}
